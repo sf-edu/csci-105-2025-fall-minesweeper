77173321619de91622c70aa610c62b2d2b141875
Isaac Corbrey <icorbrey@gmail.com>
2025-11-06 16:54:49 -0500

    Store spaces in the board

--- Program.cs
+++ Program.cs
@@ -2,12 +2,26 @@ var board = new Board(30, 20);
 
 class Board
 {
+    public Space[] Spaces { get; set; }
     public int Height { get; set; }
     public int Width { get; set; }
+
     public Board(int width, int height)
     {
+        Spaces = new Space[width * height];
         Height = height;
         Width = width;
+
+        for (var i = 0; i < Spaces.Length; i++)
+        {
+            Spaces[i] = new Space();
+        }
+    }
+
+    public Space GetSpace(int x, int y)
+    {
+        return Spaces[x + y * Width];
+    }
     }
 }
 
