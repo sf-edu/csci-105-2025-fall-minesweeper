a4059624c40ea3f8ebf829ac2731e56d0532dbc7
Isaac Corbrey <icorbrey@gmail.com>
2025-11-06 16:55:59 -0500

    Place bombs on the board

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 var board = new Board(30, 20);
+board.PlaceBombs(99);
 
 class Board
 {
@@ -21,6 +22,33 @@ class Board
     public Space GetSpace(int x, int y)
     {
         return Spaces[x + y * Width];
+    }
+
+    public void PlaceBombs(int count)
+    {
+        var random = new Random();
+
+        for (var i = 0; i < count; i++)
+        {
+            PlaceBomb(random);
+        }
+    }
+
+    public void PlaceBomb(Random random)
+    {
+        while (true)
+        {
+            var x = random.Next(Width);
+            var y = random.Next(Height);
+
+            var space = GetSpace(x, y);
+
+            if (!space.IsBomb)
+            {
+                space.IsBomb = true;
+                return;
+            }
+        }
     }
     }
 }
